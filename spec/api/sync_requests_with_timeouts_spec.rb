c2505a2c60773fe48615311ea09a12697cc9b04d
Roberto Ostinelli <roberto@ostinelli.net>
2016-04-28 20:44:09 +0200

    Add timeout tests for sync requests.

---
+++ spec/api/sync_requests_with_timeouts_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+describe "Sync requests with timeouts" do
+  let(:port) { 9516 }
+  let(:server) { NetHttp2::Dummy::Server.new(port: port) }
+  let(:client) { NetHttp2::Client.new(uri: "http://localhost:#{port}") }
+
+  before do
+    server.listen
+    server.on_req = Proc.new { |_req| sleep 2 }
+  end
+
+  after do
+    client.close
+    server.stop
+  end
+
+  it "returns nil when no response is received within the specified timeout" do
+    response = client.get('/path', { 'x-custom-header' => 'custom' }, timeout: 1)
+
+    expect(response).to be_nil
+  end
+
+  it "returns nil when no sequential responses are received within the specified timeout" do
+    responses = []
+    responses << client.get('/path', { 'x-custom-header' => 'custom' }, timeout: 1)
+    responses << client.get('/path', { 'x-custom-header' => 'custom' }, timeout: 1)
+
+    expect(responses.compact).to be_empty
+  end
+
+  it "returns nil when no concurrent responses are received within the specified timeout" do
+    started_at = Time.now
+
+    responses = []
+    thread = Thread.new { responses << client.get('/path', { 'x-custom-header' => 'custom' }, timeout: 1) }
+    responses << client.get('/path', { 'x-custom-header' => 'custom' }, timeout: 1)
+
+    thread.join
+
+    time_taken = Time.now - started_at
+    expect(time_taken < 2).to eq true
+
+    expect(responses.compact).to be_empty
+  end
+end
