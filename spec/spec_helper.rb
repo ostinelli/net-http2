e62ef6de0bea671ad3012a368d3ba81acd09d257
Roberto Ostinelli <roberto@ostinelli.net>
2016-12-29 20:45:23 +0100

    Use random order in tests.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,5 +6,6 @@ require 'rspec'
 Dir[File.expand_path("../support/**/*.rb", __FILE__)].each { |f| require f }
 
 RSpec.configure do |config|
+  config.order = :random
   config.include NetHttp2::ApiHelpers
 end
